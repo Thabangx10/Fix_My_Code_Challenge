010ab61adc2d971251065df7e6b9de4ff2cbb33b
Thabangx10 <Thabangx10@gmail.com>
2023-02-19 01:47:25 -0800

    0

--- 0x00-challenge/0-fizzbuzz.py
+++ 0x00-challenge/0-fizzbuzz.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
-"""
-FizzBuzz
+""" FizzBuzz
+    Change of logic if (i % 3) == 0 and (i % 5) == 0:
 """
 import sys
 
 
 def fizzbuzz(n):
     """
-    The function will be printing numbers seperated (1 from n)
-
-    Multiples:
-             3 - print "Fizz"
-             5 - print "Buzz"
-             both - print "FizzBuzz"
-    Error:
-             we needed to handle the error by calling the if loop that called
-             both multiples first in the program.
+    FizzBuzz function prints numbers from 1 to n separated by a space.
+    
+    - For multiples of three print "Fizz" instead of the number and for
+      multiples of five print "Buzz".
+    - For numbers which are multiples of both three and five print "FizzBuzz".
     """
     if n < 1:
         return
@@ -23,13 +19,13 @@ def fizzbuzz(n):
     tmp_result = []
     for i in range(1, n + 1):
         if (i % 3) == 0 and (i % 5) == 0:
-            temp_result.append("FizzBuzz")
+            tmp_result.append("FizzBuzz")
         elif (i % 3) == 0:
-            temp_result.append("Fizz")
+            tmp_result.append("Fizz")
         elif (i % 5) == 0:
-            temp_result.append("Buzz")
+            tmp_result.append("Buzz")
         else:
-            temp_result.append(str(i))
+            tmp_result.append(str(i))
     print(" ".join(tmp_result))
 
 
@@ -40,5 +36,5 @@ if __name__ == '__main__':
         print("Example: ./0-fizzbuzz.py 89")
         sys.exit(1)
 
-    number = int(sys,argv[1])
+    number = int(sys.argv[1])
     fizzbuzz(number)
